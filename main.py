be84e1f9d48a39279cf4245fa2102462f4704aac
Oliver Stein <oliver.stein@cern.ch>
2015-07-06 10:06:30 +0200

    mino changes to scropt

--- main.py
+++ main.py
@@ -39,12 +39,15 @@ p = plotter()
 
 
 cwd = '/Users/Oli/work/dBLM_readout/LHC_data_analysis_scope'
-
+test_env='/Users/Oli/work/dBLM_readout/LHC_data_analysis_scope_test_env'
+# cwd = test_env
 data_fold = 'data'
 plot_fold = 'plots' 
 ana_res_fold = 'ana_res' 
 
 data_path  = '/Users/Oli/work/dBLM_readout/IP2/data/data'
+data_test_env  = '/Users/Oli/work/dBLM_readout/IP2/data_test_env/data'
+# data_path  = data_test_env
 # data_path = os.path.join(cwd,data_fold)
 
 
@@ -74,6 +77,7 @@ i.data_list_creator('.txt',pflag)
 p.set_save_path(plot_path,pflag)
 ar.infrastruc(ana_res_path,pflag)
 
+
 header=[['time_stamp','year','month','day','hour','minute','second','time sec','time zero',
          'delta time zero','beam','ip','loc','dcum','type','daq_name','channel','mode','beamstatus',
          't total','t start','t end','t delta','offset','offset_corr','noise','sig 10 pt','sig 10 t','sig 50 pt','sig 50 t','run','analysed',
@@ -95,14 +99,25 @@ scope_conf = [['daq_name'        ,'ip'   ,'beam' ,'loc'  ,'dcum'     ,'type','ch
              ]
  
 ar.header_set(header,pflag)
+# print ar.haeder
+print i.data_list
+ar.int_data_sets = ar.file_setup(ana_res_path,'int_data_sets.csv',pflag)
+# print ar.file_setup(ana_res_path,'ana_file.csv',pflag)
+ar.ana_file =  ar.file_setup(ana_res_path,'ana_file.csv',pflag)
+ar.ana_file_updater(i.data_list,pflag)
+ar.ana_file_saver(1,pflag)# print ar.ana_file
+# print ar.int_data_sets
+print ar.ana_file
 # sys.exit('stop')
-ar.ana_file_check(1,pflag)
-ar.ana_file_loader(pflag)
-ar.ana_file_writer(i.data_list,pflag)
-ar.ana_file_saver(1,pflag)
+# ar.ana_file_check(0,pflag)
+# ar.ana_file_loader(pflag)
+# ar.ana_file_writer(i.data_list,pflag)
+# ar.ana_file_saver(1,pflag)
 
 # sys.exit('stop')
 max_it = len(ar.ana_file)
+force_flag = 0
+# max_it = 2
 # print ar.ana_file[0]
 
 a.header(ar.ana_file[0],pflag)
@@ -114,7 +129,8 @@ for k in range(1,max_it):
     g.loop_info(k,i.len_data_list,pflag)
     print ar.ana_file[k]
     line = ar.ana_file[k]
-    ana_flag = a.tba_check(line,1,pflag)
+    ana_flag = a.tba_check(line,force_flag,pflag)
+    g.printer(ana_flag,pflag)
     if ana_flag == 1:
         g.tprinter('start analysis',pflag)
         a.name_info_scope(line,scope_conf,pflag)
@@ -130,8 +146,10 @@ for k in range(1,max_it):
         a.sig_above(line,a.data,10,pflag)
         a.sig_above(line,a.data,50,pflag)
         
-        
-        
+#         p.simple_plotter(line,a.data,a.header,1,pflag)
+        p.simple_plotter_zoom(line,a.data,a.header,a.time_stamp_string,-1e-6,1e-6,1,pflag)
+        p.simple_plotter_zoom(line,a.data,a.header,a.time_stamp_string,-1e-7,5e-7,1,pflag)
+        p.simple_plotter_zoom(line,a.data,a.header,a.time_stamp_string,-1e-6,100e-6,1,pflag)
 #         a.bin_time_cor(pflag)
 #         a.c_counter(line,pflag)
 #         a.set_t_zero(line,pflag)
@@ -152,7 +170,9 @@ for k in range(1,max_it):
     
     
     a.analysed(ar.ana_file[k],pflag)
- 
+
+# ar.int_data_cond(line,'sig 50 pt',1,1,pflag)
+# ar.file_saver(ana_res_path,'int_data_sets.csv',pflag) 
 ar.ana_file_saver(1,pflag)   
     
 # for i in ar.ana_file:
